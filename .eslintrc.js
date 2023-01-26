1d9e13d87b7194751d9c8baa2fa136af2a3d1f19
kalmonJ <kalmonkk69@gmail.com>
2023-01-26 12:41:28 -0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ['plugin:hydrogen/recommended', 'plugin:hydrogen/typescript'],
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     'node/no-missing-import': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/naming-convention': 'off',
